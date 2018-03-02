08341ad57e3cc58a073ff7fac7857ea95c211124
meng <Annielm1996@gmail.com>
2018-03-02 11:28:32 -0500

    1st

--- meng/simplestore/products/apps.py
+++
@@ -1,9 +0,0 @@
-from django import apps
-
-
-class AppConfig(apps.AppConfig):
-    name = "simplestore.products"
-
-    def ready(self):
-        # noinspection PyUnresolvedReferences
-        from . import signals
--- meng/simplestore/static/js/main.js
+++
@@ -1,8 +0,0 @@
-const blazy = require('blazy');
-import 'jquery';
-import 'bootstrap/dist/js/bootstrap.js';
-import 'bootstrap-cosmo.css';
-import 'app.sass';
-
-
-const lazyImages = new blazy();
