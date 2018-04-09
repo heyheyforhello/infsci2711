6405ef9e1d3a5e51c21b87d834e3039481e63ca3
EndEdge <bi.jk@hotmail.com>
2018-04-09 12:31:24 -0400

    Add files via upload
    A edited version of Logan's code for adjusting the star schema.

---
+++ bi/Dataloading/loadData.py
@@ -0,0 +1,41 @@
+import os
+import py2neo
+import json
+
+# NOTES
+# script located in project folder
+# JSON data located in /data/
+# Run python SimpleHTTPServer in project directory
+# In Neo4j, created new project named "2711 Project"
+# Inside that project created new database "yelpData" with password 'password'
+
+# Connect to neo4j
+neo4jUrl = "http://localhost:7474/db/data"
+graph = py2neo.Graph(neo4jUrl, password='123456')
+print("neo4j connected")
+
+# This is what counts as defining a schema
+#graph.schema.create_uniqueness_constraint('Business', 'business_id')
+#graph.schema.create_uniqueness_constraint('Category', 'name')
+#graph.schema.create_uniqueness_constraint('User', 'user_id')
+#print "constraints added"
+
+# Load data in chunks of 10000
+for dataType in ['user', 'business', 'checkin', 'review']:
+  print('loading ' + dataType)
+  with open ('load_' + dataType + '.cql') as queryFile:
+    query = queryFile.read()
+  with open('data/'+ dataType + '.json', encoding='utf-8') as dataFile:
+    counter = 0
+    acc = []
+    for b in dataFile:
+      counter += 1
+      acc.append(json.loads(b))
+      if counter == 1000:
+        result = graph.run(query, json={'data': acc})
+        print('database update ' + dataType + ':' + str(result.next()['count']))
+        acc = []
+        counter = 0
+    result = graph.run(query, json={'data': acc})
+    print('database update ' + dataType + ':' + str(result.next()['count']))
+  print('finished ' + dataType)
